4c571c4cd53dbd82555ed6fa3752ceecc7616145
lucacappello79 <postt79@gmail.com>
2023-03-05 23:38:39 +0100

    feat: attempting bonus

--- js/script.js
+++ js/script.js
@@ -1,36 +1,78 @@
 let gridContainerEl = document.getElementById("grid-container");
 let startButtonEl = document.getElementById("start");
-let cellTotal = 100
+let difficultyEl = document.getElementById("difficulty")
 let newCellEl;
 
 gridContainerEl.style.display = "none";
 
+
 startButtonEl.addEventListener("click", function(){
 
     gridContainerEl.style.display = "flex";
+    //oppure 
+    //gridContainerEl.innerHTML o innerText = "";
 
-    for (let i = 0; i < cellTotal; i++) {
+    
+    let difficulty = difficultyEl.value
+
+    if (difficulty == "Normal") {
+
+        let cellTotal = 100;
+
+        //se non do let i = 1; e i < cellTotal + 1; a questo ciclo for partirà a numerarmile celle da 0 a 99
+        for (let i = 1; i < cellTotal + 1; i++) {
 
         let newCellEl = generateCell(i);
 
-        
         newCellEl.addEventListener("click", function(){
             
             newCellEl.classList.toggle("blue");
+             console.log(i);
             
         })
 
         gridContainerEl.append(newCellEl);
 
+
+    }} else if (difficulty == "Hard") {
+
+        let cellTotal = 81;
+        
+        for (let i = 1; i < cellTotal + 1; i++) {
+    
+            let newCellEl = generateCell(i);
+        
+            newCellEl.addEventListener("click", function(){
+                
+                newCellEl.classList.toggle("blue");
+                 console.log(i);
+                
+            })
+    
+            gridContainerEl.append(newCellEl);
+    
+        }} else if (difficulty == "Champion") {
+        
+        let cellTotal = 49;
+        
+        for (let i = 1; i < cellTotal + 1; i++) {
+    
+            let newCellEl = generateCell(i);
+        
+            newCellEl.addEventListener("click", function(){
+                
+                newCellEl.classList.toggle("blue");
+                 console.log(i);
+                
+            })
+    
+            gridContainerEl.append(newCellEl);
+    
+        }
+
     }
-
-
+    
 })
-
-
-
-
-
 
 
 function generateCell(number) {
@@ -46,3 +88,6 @@ function generateCell(number) {
     return cell;
 
 }
+
+
+
